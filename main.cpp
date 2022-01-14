5f4841e83999dfe213208d0775a9d016d0004bda
Смирнов Никита <smirnov-2000-2000@list.ru>
2022-01-14 09:44:06 +0700

    структуры

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <QCoreApplication>
+#include <iostream>
 
-int main(int argc, char *argv[])
-{
+// описание структуры
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+//функция вывода даты на экран
+void print(Date d) {
+std::cout << d.day << "." << d.month
+          << "." << d.year << std::endl;
+
+}
+
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
+    //Cоздаём объект
+    Date today;
+    //Инициализируем поля
+    today.day = 14;
+    today.month = 1;
+    today.year = 2022;
+
+    //Выводим на экран
+    print(today);
     return a.exec();
 }
